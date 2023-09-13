4798312b75c4d9a502d35373924a7f4acb14410d
last <ramses.aldama@gmail.com>
2023-09-13 05:27:32 -0700

    first commit

---
+++ init.lua
@@ -0,0 +1,3 @@
+require("basics")
+require("plugin-manager")
+
---
+++ lua/plugins/projectlaunch.lua
@@ -0,0 +1,8 @@
+return {
+    "sheodox/projectlaunch.nvim",
+    config = function()
+      require("projectlaunch").setup()
+      -- open the main menu
+      vim.api.nvim_set_keymap('n', "<leader>ll", "<cmd>lua require('projectlaunch').toggle_main_menu()<cr>", { noremap = true, silent = true })
+    end,
+}
