f4980028e29b21a555724fa1eddc6df67fe85150
last <ramses.aldama@gmail.com>
2023-09-16 06:32:28 -0700

    update

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -21,14 +21,23 @@ return {
       }
     })
 
-    mason_lspconfig.setup({
+    -- mason_lspconfig.setup({
       -- list of servers for mason to install
-      ensure_installed = {
-        "lua_ls",
-      },
+      -- ensure_installed = {
+      --   "tsserver",
+      --   "html",
+      --   "cssls",
+      --   "tailwindcss",
+      --   "svelte",
+      --   "lua_ls",
+      --   "graphql",
+      --   "emmet_ls",
+      --   "prismals",
+      --   "pyright"
+      -- },
       -- auto-install configured servers (with lspconfig)
-      automatic_installation = true, -- not the same as ensure_installed
-    })
+      -- automatic_installation = true, -- not the same as ensure_installed
+    -- })
 
   end,
 }
