348b37304538f623bdddd0f1d6483374be69f5c9
last <ramses.aldama@gmail.com>
2023-09-29 10:49:34 -0700

    update

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -2,12 +2,13 @@ return {
    "hrsh7th/nvim-cmp",
    event = "InsertEnter",
    dependencies = {
-      "hrsh7th/cmp-buffer",         -- source for text in buffer
-      "hrsh7th/cmp-path",           -- source for file system paths
-      "L3MON4D3/LuaSnip",           -- snippet engine
-      "saadparwaiz1/cmp_luasnip",   -- for autocompletion
+      "hrsh7th/cmp-buffer",           -- source for text in buffer
+      "hrsh7th/cmp-path",             -- source for file system paths
+      "hrsh7th/cmp-nvim-lsp",         -- adds LSP completion capabilities
+      "L3MON4D3/LuaSnip",             -- snippet engine
+      "saadparwaiz1/cmp_luasnip",     -- for autocompletion
       "rafamadriz/friendly-snippets", -- useful snippets
-      "onsails/lspkind.nvim",       -- vs-code like pictograms
+      "onsails/lspkind.nvim",         -- vs-code like pictograms
    },
    config = function()
       local cmp = require("cmp")
@@ -19,6 +20,12 @@ return {
       -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
       require("luasnip.loaders.from_vscode").lazy_load()
 
+      local has_words_before = function()
+         unpack = unpack or table.unpack
+         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
       cmp.setup({
          completion = {
             completeopt = "menu,menuone,preview,noselect",
@@ -28,21 +35,48 @@ return {
                luasnip.lsp_expand(args.body)
             end,
          },
-         mapping = cmp.mapping.preset.insert({
+         mapping = {
             ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
             ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
             ["<C-b>"] = cmp.mapping.scroll_docs(-4),
             ["<C-f>"] = cmp.mapping.scroll_docs(4),
             ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-            ["<C-e>"] = cmp.mapping.abort(),    -- close completion window
+            ["<C-e>"] = cmp.mapping.abort(),        -- close completion window
             ["<CR>"] = cmp.mapping.confirm({ select = false }),
-         }),
+            -- ... Your other mappings ...
+
+            ["<Tab>"] = cmp.mapping(function(fallback)
+               if cmp.visible() then
+                  cmp.select_next_item()
+                  -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+                  -- they way you will only jump inside the snippet region
+               elseif luasnip.expand_or_jumpable() then
+                  luasnip.expand_or_jump()
+               elseif has_words_before() then
+                  cmp.complete()
+               else
+                  fallback()
+               end
+            end, { "i", "s" }),
+
+            ["<S-Tab>"] = cmp.mapping(function(fallback)
+               if cmp.visible() then
+                  cmp.select_prev_item()
+               elseif luasnip.jumpable(-1) then
+                  luasnip.jump(-1)
+               else
+                  fallback()
+               end
+            end, { "i", "s" }),
+
+            -- ... Your other mappings ...
+         },
          -- sources for autocompletion
          sources = cmp.config.sources({
             { name = "nvim_lsp" },
             { name = "luasnip" }, -- snippets
-            { name = "buffer" }, -- text within current buffer
-            { name = "path" }, -- file system paths
+            { name = "buffer" },  -- text within current buffer
+            { name = "path" },    -- file system paths
          }),
          -- configure lspkind for vs-code like pictograms in completion menu
          formatting = {
--- lua/plugins/telescope-file-browser.lua
+++ lua/plugins/telescope-file-browser.lua
@@ -3,6 +3,7 @@ return {
    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
    config = function()
       local fb_actions = require "telescope._extensions.file_browser.actions"
+      local actions = require "telescope.actions"
       require("telescope").setup {
          extensions = {
             file_browser = {
@@ -13,6 +14,11 @@ return {
                display_stat = false,
                grouped = true,
                hide_parent_dir = true,
+               file_browser = {
+                  preview = {
+                     ls_short = true,
+                  }
+               },
                mappings = {
                   ["n"] = {
                      ["n"] = fb_actions.create,
@@ -21,7 +27,7 @@ return {
                      ["y"] = fb_actions.copy,
                      ["h"] = fb_actions.goto_parent_dir,
                      ["."] = fb_actions.toggle_hidden,
-                     ["l"] = require('telescope.actions').select_default,
+                     ["l"] = actions.select_default,
                   },
                },
             },
